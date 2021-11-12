aff5733da60b669b27fb16150b9e71692e11d6b3
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-12 19:15:21 +0000

    change secret key

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = "mysecretbookengine";
 const expiration = '2h';
 
 module.exports = {
