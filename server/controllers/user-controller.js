7f6ad1de2c3fe6713dc8a930941a323788dff443
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-12 18:39:39 +0000

    chnage the mail to lowercase in the server

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -18,7 +18,9 @@ module.exports = {
   },
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
   async createUser({ body }, res) {
-    const user = await User.create(body);
+    let email = body.email.toLowerCase();
+    let newUser = { ...body, email };
+    const user = await User.create(newUser);
 
     if (!user) {
       return res.status(400).json({ message: 'Something is wrong!' });
