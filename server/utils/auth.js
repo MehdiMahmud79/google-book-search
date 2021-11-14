0a371675c4841ffcc19ee7048859df97982f846e
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-14 13:19:17 +0000

    fix contect to make token

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,18 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (req, res) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
+      // .status(400).json({ message: "You have no token!" });
     }
 
     // verify token and get user data out of it
@@ -24,12 +25,11 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log("Invalid token");
+      // return res.status(400).json({ message: "invalid token!" });
     }
-
+    return req;
     // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
