992d210e6bb1c7d12ba9567e2c34e9ef4009d4cd
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-14 13:20:13 +0000

    add types, queries and mutation to thetypeDef

---
+++ server/graphqlSchema/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  input Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type SavedBook {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: String
+    savedBooks: [SavedBook]
+  }
+
+  type Login {
+    token: String!
+    user: User!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Login
+    signUp(username: String!, email: String!, password: String!): Login
+    saveBook(input: Book!): User
+    deleteBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
