e41a504654c4f9347e6589598826f3438194bfae
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-14 23:05:45 +0000

    add queries for gql

---
+++ client/src/utils/queries.js
@@ -0,0 +1,90 @@
+import { gql } from "@apollo/client";
+
+export const signUpMutation = gql`
+  mutation signUp($username: String!, $email: String!, $password: String!) {
+    signUp(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const getMe = gql`
+  query getMe {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+// mutation queries to GraphQl
+export const loginUser = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        savedBooks {
+          authors
+          description
+          bookId
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
+
+export const saveBook = gql`
+  mutation saveBook($book: BookTosave!) {
+    saveBook(book: $book) {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const deleteBook = gql`
+  mutation deleteBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
