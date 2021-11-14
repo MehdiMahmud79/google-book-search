7845250a62018597162295ff0be59c17e5208d59
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-14 23:07:05 +0000

    setup  aopolo client  server

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import {
+  ApolloProvider,
+  ApolloLink,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  concat,
+} from "@apollo/client";
+
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+import Auth from "./utils/auth";
 
 function App() {
+  const httpLink = new HttpLink({ uri: "/graphql" });
+  const authMiddleware = new ApolloLink((operation, forward) => {
+    // add the authorization to the headers
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        authorization: Auth.loggedIn() ? Auth.getToken() : null,
+      },
+    }));
+
+    return forward(operation);
+  });
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: concat(authMiddleware, httpLink),
+  });
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
-
 export default App;
