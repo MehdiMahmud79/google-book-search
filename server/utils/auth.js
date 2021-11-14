1777480af194805de5ec05f38f96b2e89de4659c
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-14 20:18:01 +0000

    remove console log from AUTH

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,8 +7,6 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res) {
-    console.log(req.headers.authorization);
-
     // allows token to be sent via  req.query or headers
     let token = req.query?.token || req.headers?.authorization;
 
@@ -28,7 +26,7 @@ module.exports = {
     }
     return req;
   },
-  
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
