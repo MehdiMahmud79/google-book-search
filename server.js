4b44f866daa23029be433c4be2309385aec2db5c
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-15 19:18:01 +0000

    fix dependecies

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 db.once("open", () => {
