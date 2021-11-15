41de3aa9c6123a86b7a0458e9352a88f98e74f0b
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-15 12:34:31 +0000

    Updates

---
+++ precache-manifest.0d5704510dc1cb12dd61cba95d4e2d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbb2e19c0ccf7af363ff8c8e831d2ec",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "d54da166e6bb9c886d93",
+    "url": "/google-book-search/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "81a41e81872fa8ee6b44",
+    "url": "/google-book-search/static/css/main.8e4bf931.chunk.css"
+  },
+  {
+    "revision": "d54da166e6bb9c886d93",
+    "url": "/google-book-search/static/js/2.0177f3bf.chunk.js"
+  },
+  {
+    "revision": "c716832b65efef799ab0bd78254edda8",
+    "url": "/google-book-search/static/js/2.0177f3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a41e81872fa8ee6b44",
+    "url": "/google-book-search/static/js/main.2ac7c6c4.chunk.js"
+  },
+  {
+    "revision": "e6ab782d36e8fb3e5b98",
+    "url": "/google-book-search/static/js/runtime-main.7807af47.js"
+  }
+]);
