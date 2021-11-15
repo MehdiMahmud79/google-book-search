b1b9ed0435280ed540148d52af375caa2aeca954
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-15 19:38:33 +0000

    initilize

---
+++ public/precache-manifest.0fd143ac0efbc770eae64b24736fab30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4577df347bcca321307e052a863266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c67ca0ec22046e14cf",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "c6bed61f3cf9bc7de12e",
+    "url": "/static/css/main.c34b173c.chunk.css"
+  },
+  {
+    "revision": "03c67ca0ec22046e14cf",
+    "url": "/static/js/2.c22ff7e8.chunk.js"
+  },
+  {
+    "revision": "c716832b65efef799ab0bd78254edda8",
+    "url": "/static/js/2.c22ff7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bed61f3cf9bc7de12e",
+    "url": "/static/js/main.df3040aa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -19,10 +19,14 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
