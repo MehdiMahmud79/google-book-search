a0f3538c636bf123c0eb25aec8b64454aba30c0c
Mehdi <84668631+MehdiMahmud79@users.noreply.github.com>
2021-11-28 00:03:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static("public"));
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "public")));
 }
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
